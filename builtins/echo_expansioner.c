99743dea081d4aa8707ecf55cb48658c95a53a1f
Thiago Guimaraes <thguimar@c2r6s7.42lisboa.com>
2024-08-21 20:14:38 +0100

    day 28

--- builtins/echo_expansioner.c
+++ builtins/echo_expansioner.c
@@ -6,12 +6,27 @@
 /*   By: thguimar <thguimar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:38:35 by thiago-camp       #+#    #+#             */
-/*   Updated: 2024/08/20 19:19:49 by thguimar         ###   ########.fr       */
+/*   Updated: 2024/08/21 19:08:20 by thguimar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libs/builtins.h"
 #include "../libft/libft.h"
+
+bool	is_only_pipe(char *str)
+{
+	int	i;
+
+	i = -1;
+	if (!str)
+		return (false);
+	while (str[++i])
+	{
+		if (str[i] != '|')
+			return (false);
+	}
+	return (true);
+}
 
 bool	is_there_pipe(char *str)
 {
@@ -52,31 +67,44 @@ char	**pipping_commands2(t_shell *utils, int x)
 
 
 	j = -1;
-	i = -1;
+	i = 0;
 	k = 0;
 	rtn = ft_calloc(sizeof(char *), x + 1);
 	while (utils->command[++j])
 	{
-		if (is_there_pipe(utils->command[j]) == false && utils->command[j][0] != '|')
+		//printf(" this one here: %s\n", utils->command[j]);
+		i = 0;
+		/*if (is_there_pipe(utils->command[j]) == false || utils->command[j][i] == '|')
+		{*/
+		if (utils->command[j][i] != '\0' && utils->command[j][i] == '|')
 		{
-			ns = size_before_pipe(utils->command[j + 1]);
-			tmp = ft_substr(utils->command[j + 1], 0, ns);
-			tmp2 = ft_strjoinn(utils->command[j], tmp);
-			printf("AAAAAAAAAA: %s,   ns: %i\n", tmp, ns);
-			free (tmp);
-			printf("BBBBBBBBBB: %s\n========================\n", tmp2);
-			if (rtn[k] && rtn[k][0])
-				free(rtn[k]);
-			rtn[k] = ft_strdup(tmp2);
-			if (is_there_pipe(utils->command[j + 1]) == true)
-			{
-				k++;
-				free(tmp2);
-			}
+			while (utils->command[j][i] == '|')
+				i++;
 		}
+		ns = size_before_pipe(utils->command[j + 1]);
+		tmp = ft_substr(utils->command[j + 1], 0, ns);
+		tmp2 = ft_strjoinn(utils->command[j] + i, tmp);
+		if (utils->command[j][i] && is_there_pipe(utils->command[j + 1]) == false && is_there_pipe(utils->command[j]) == false)
+			j++;
+		//printf("AAAAAAAAAA: %s\n", tmp);
+		free (tmp);
+	//	printf("BBBBBBBBBB: %s\n========================\n", tmp2);
+		if (rtn[k] && rtn[k][0])
+			free(rtn[k]);
+		rtn[k] = ft_strdup(tmp2);
+		if (utils->command[j] && utils->command[j + 1] && utils->command[j + 1][0] != '|')
+		{
+			k++;
+			//if (utils->command[j + 1][1] == '\0')
+			j++;
+			free(tmp2);
+		}
+		if (is_there_pipe(utils->command[j + 1]) == true)
+			k++;
+		if (is_only_pipe(utils->command[j + 1]) == true)
+			j++;
+		//}
 	}
-	if (tmp2)
-		free(tmp2);
 	j = -1;
 	while (rtn[++j])
 		printf("CCCCCCCCCCC: %s\n", rtn[j]);
